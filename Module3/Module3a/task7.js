6537c407d95e2d822562ec556941f92134fe78c0
unknown <adeboyeifeoluwa1@gmail.com>
2023-10-25 20:05:26 +0100

    new tasks

--- Module3/Module3a/task7.js
+++ Module3/Module3a/task7.js
@@ -2,7 +2,7 @@ const side1 = 66;
 const side2 = 66;
 const side3 = 66;
 
-if ((side1 === side2) && (side2=== side3)){
+if ((side1 === side2 && side2=== side3)){
     console.log("Equilateral triangle")
 }
 else if (side1 === side2 || side2 === side3 || side1 === side3) {
---
+++ Module3/Module3a/task8.js
@@ -0,0 +1,6 @@
+for (let number = 1; number <= 20; number = number+1){
+    if (number % 2 !== 0) {
+        console.log(number + " is an odd number")
+    }
+
+}
