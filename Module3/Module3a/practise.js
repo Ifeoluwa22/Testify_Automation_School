4f620ad0195a45bf55d43185c436a3c957d1901a
unknown <adeboyeifeoluwa1@gmail.com>
2023-10-26 11:00:32 +0100

    New tasks

---
+++ Module3/Module3a/practise.js
@@ -0,0 +1,11 @@
+function converter(dollars, naira) {
+    let rate = 1200
+    const nairaRate = dollars * rate
+    //const dollarRate = naira / 1200
+
+    return(nairaRate)
+}
+
+console.log(converter(500))
+
+//this is just to convert naira to dollars
---
+++ Module3/Module3a/task9.js
@@ -0,0 +1,14 @@
+// function calculateAreaOfRectangle(length, width, product) {
+//     length = 20
+//     width = 3
+//     product = length * width
+//     console.log(product)
+// } calculateAreaOfRectangle(); This is one way to do it. The way I initially assumed. The correct answer is gotten but the syntax isn't best practise
+
+function calculateAreaOfRectangle(length,width){
+    const product = length * width;
+    console.log(product)
+}
+
+
+calculateAreaOfRectangle(10, 5);
