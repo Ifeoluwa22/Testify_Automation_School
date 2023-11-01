7cf0ea74178a2292241ac9b6871891ca4314289b
unknown <adeboyeifeoluwa1@gmail.com>
2023-11-02 00:38:07 +0100

    lesson 36

---
+++ Module3/Module3a/task14.js
@@ -0,0 +1,16 @@
+const books = {
+    title: "The secret lives of Baba Segi's wives", 
+    description: "A book written by Nigerian author Lola Shoneyin about a man with 3 wives who fought over his inheritance after his death", 
+    numberOfPages: 245,
+    authour: "Lola Shoneyin",
+    reading: true,
+    toggleReadingStatus: function(){
+        if(books.reading === true){
+            books.reading === false
+        } else{
+            books.reading === true
+        }
+    }
+ }
+
+ console.log(books.title)
