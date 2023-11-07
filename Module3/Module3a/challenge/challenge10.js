acc2192980bf5af2b4329f61b7d3d53f79dffd76
unknown <adeboyeifeoluwa1@gmail.com>
2023-11-08 00:09:59 +0100

    update

--- Module3/Module3a/challenge/challenge10.js
+++ Module3/Module3a/challenge/challenge10.js
@@ -2,6 +2,10 @@
 
 let numbers = [2, 6, -8, 90, 18, -54, -33, 27, -29, 7, -4]
 
-function name(params) {
-    
+function newFunc(positiveNumbers) {
+    return positiveNumbers > 0
 }
+
+let newNumbers = numbers.filter(newFunc)
+
+console.log(newNumbers)
