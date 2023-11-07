d2e24865853f5af565ab30ce359abc3271bd55a7
unknown <adeboyeifeoluwa1@gmail.com>
2023-11-08 00:51:26 +0100

    update

--- Module3/Module3a/challenge/challenge7.js
+++ Module3/Module3a/challenge/challenge7.js
@@ -1,2 +1,13 @@
 // Sort an array of numbers in descending order
 
+let ascending = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+
+let descending = [ ]
+
+for (let i = ascending.length - 1; i >= 0; i--) {
+    const element = ascending[i];
+    descending.push(element)   
+}
+console.log(descending)
+
+//Check how to use pop
