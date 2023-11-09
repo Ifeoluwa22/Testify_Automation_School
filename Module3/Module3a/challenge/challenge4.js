a215ff04a3cc53704910f6122ffa7b7a49c9fa65
unknown <adeboyeifeoluwa1@gmail.com>
2023-11-09 23:56:27 +0100

    last one

--- Module3/Module3a/challenge/challenge4.js
+++ Module3/Module3a/challenge/challenge4.js
@@ -1,6 +1,6 @@
 //Print a table containing multiplication tables
 function multiplication(number) {
-    for (let i = 0; i <= 12; i++) {
+    for (let i = 1; i <= 12; i++) {
         let multi = number * i
         console.log("The multiplication table for " + number + " is " + number + " x " + i + " = " + multi )
         
