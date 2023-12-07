b8b1c162160e0f31dbfc994cce94938c9ccca77a
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2023-12-07 02:36:07 +0100

    neww

---
+++ Module4/task1/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Java is supposedly fun");
+        System.out.println("There are various programming languages.");
+        System.out.println("Testing is awesome!");// write your code here
+    }
+}
