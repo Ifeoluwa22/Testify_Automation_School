bed50ab2b4202934d67523766869fd0eaaa06685
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2023-12-15 11:59:06 +0100

    Finally

---
+++ Module4/Tasks/com/testify/Task11/ClassB.java
@@ -0,0 +1,15 @@
+package com.testify.Task11;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+
+       ClassA user = new ClassA();
+        user.name = "Anastasia";
+
+        user.nameBank(user.name);
+
+    }
+
+}
+
---
+++ Module4/Tasks/com/testify/Task12/ClassA/ClassA.java
@@ -0,0 +1,21 @@
+package com.testify.Task12.ClassA;
+import com.testify.Task12.ClassB.ClassB;
+
+
+public class ClassA {
+
+    //public static void main(String[] args) {
+    //ClassB Glasses = new ClassB();
+//}
+
+    public void program(){
+        System.out.println("Welcome to my program");
+
+
+    }
+
+}
+//Create two packages. in each of the packages create one class each Class A and Class B.
+// create a method in class A that can be accessed anywhere in that project and
+// create a method in Class B that can only be accessed within a class.
+
