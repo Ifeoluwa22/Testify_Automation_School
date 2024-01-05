578132ebc6a3b15e60311fc5d89a5bee219cd93c
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-01-05 10:09:51 +0100

    thanks

--- Module4/Tasks/challenge/challenge4.java
+++ Module4/Tasks/challenge/challenge4.java
@@ -5,20 +5,20 @@ public class challenge4 {
         System.out.println(" The Prime numbers between 1 and 100 are: ");
 
         for (int i = 1; i <= 100; i++) {
-            if (isPrime(i)) {
+            if (primeNo(i)) {
                 System.out.print(i + ", ");
             }
         }
     }
 
     // Function to check if a number is prime
-    private static boolean isPrime(int num) {
-        if (num <= 0) {
+    private static boolean primeNo(int number) {
+        if (number <= 0) {
             return false;
         }
 
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
--- Module4/Tasks/challenge/challenge7.java
+++ Module4/Tasks/challenge/challenge7.java
@@ -1,23 +1,23 @@
 package challenge;
 
 public class challenge7 {
-    public static void main(String[] args) {
-        String original = "TestifyAutomation";
 
-        String reversed = reverseString(original);
-
-        System.out.println("Original String: " + original);
-        System.out.println("Reversed String: " + reversed);
-    }
-
-    // Function to reverse a string using recursion
-    private static String reverseString(String str) {
-        if (str.isEmpty()) {
+    // Creating a function to reverse a string using recursion like challenge 2
+    private static String reversedString(String str) {
+        if ((str == null) || (str.length()<=1)) {
             return str;
         } else {
             // Concatenate the last character with the reversed substring of the rest of the string
-            return str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
+            return str.charAt(str.length() - 1) + reversedString(str.substring(0, str.length() - 1));
         }
+    }
+
+    public static void main(String[] args) {
+        String initial = "TestifyAutomation";
+        String reversed = reversedString(initial); //recursive function call to reversedString()
+
+        System.out.println("The initial string was: " + initial); //Printing out the result
+        System.out.println("The reversed string is: " + reversed);
     }
 }
 
