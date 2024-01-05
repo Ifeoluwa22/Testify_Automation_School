f6945e05691e9ac4aa4a7adfc72fc2b413b3a7c8
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-01-05 09:52:40 +0100

    an update

--- Module4/Tasks/challenge/challenge4.java
+++ Module4/Tasks/challenge/challenge4.java
@@ -2,18 +2,18 @@ package challenge;
 
 public class challenge4 {
     public static void main(String[] args) {
-        System.out.println("Prime numbers between 1 and 100:");
+        System.out.println(" The Prime numbers between 1 and 100 are: ");
 
-        for (int i = 2; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (isPrime(i)) {
-                System.out.print(i + " ");
+                System.out.print(i + ", ");
             }
         }
     }
 
     // Function to check if a number is prime
     private static boolean isPrime(int num) {
-        if (num <= 1) {
+        if (num <= 0) {
             return false;
         }
 
