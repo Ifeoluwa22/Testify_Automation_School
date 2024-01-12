9ffc527356bc69a53c0bed91427e3bb0d0aaf52a
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-01-12 12:33:53 +0100

    thanks

---
+++ Module4B/Task/src/test/java/org/example/Task6.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task6 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\TESTIFY\\Testify_Automation_School\\Module4B\\Task\\src\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.cssSelector("#add-to-cart-sauce-labs-bike-light")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("div.page_wrapper div:nth-child(1) div.header_container:nth-child(1) div.primary_header div.shopping_cart_container:nth-child(3) > a.shopping_cart_link")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#checkout")).click();
+    }
+}
