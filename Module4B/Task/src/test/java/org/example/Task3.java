018098bb0035df196f7e9bdc63ca48971aff73ae
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-01-12 09:15:52 +0100

    up

--- Module4B/Task/src/test/java/org/example/Task3.java
+++ Module4B/Task/src/test/java/org/example/Task3.java
@@ -1,5 +1,16 @@
 package org.example;
 
-public class Task3 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Task3 {
+    public static void main(String[] args) {
+
+
+    }
 }
+
+//screenshot 1: https://monosnap.com/file/LuH3K8MZe2gRz8xGLz9JVCHAs56eFz
+// screenshot 2: https://monosnap.com/file/fq6qKAwdcY6l9fCfeqiSd0r7QaQhfy
+// screenshot 3: https://monosnap.com/file/YuM4elZ4ypWen9yfRNPN9igvcrsLOO
