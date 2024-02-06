b186e78322e3e75689494076c47c058fa1d9b7ad
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-02-07 00:04:22 +0100

    update

---
+++ Module4/Module4C/Task15.py
@@ -0,0 +1,33 @@
+fruits = ["apples", "grapes", "mangoes", "pineapple", "watermelon", "oranges"]
+print(fruits)
+
+#Sorting the list
+#ascending order
+fruits.sort()
+print("ascending order: ", fruits)
+fruits.sort(reverse=True)
+print("decending order: ", fruits)
+
+# fruits.append("Papaya")
+# print("appending", fruits)
+#
+# fruits.pop(3)
+# print("pop pineapple", fruits)
+#
+# fruits.insert(3, "apples")
+# print("add tomatoes", fruits)
+#
+# fruits.remove("grapes")
+# print("no grapes", fruits)
+#
+# total_no = fruits.count("apples")
+# print("no of apples: ", total_no)
+
+
+# Declare a list with any value and number of item/element
+# Print the list in console
+# Sort the list
+# Print the list in console again
+#
+# Note: you can experiment with the other list functions too in the task.
+
