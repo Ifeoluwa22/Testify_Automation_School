336ba71dc2ab172aed04751465c0643348f65945
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-02-08 02:18:23 +0100

    well

--- Module4/Module4C/Task16.py
+++ Module4/Module4C/Task16.py
@@ -10,10 +10,6 @@ student.update({"state of origin": "Adamawa", "sport": "basketball"})
 print(student)
 
 
-
-
-
-
 # Declare a dictionary with any key-value pair of items/elements
 # Print the dictionary in console
 # Update the dictionary with two different key-value pair items
