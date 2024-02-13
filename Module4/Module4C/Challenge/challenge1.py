b4319c9d4d0247a6f7fba955ada541b034a95b44
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-02-13 17:26:03 +0100

    arranged and challenge

---
+++ Module4/Module4C/Challenge/challenge1.py
@@ -0,0 +1,13 @@
+num = [3, 3, 9, 30, 8, 10, 23, 938, 99]
+
+
+def sum_of_numbers(number):
+    total = 0
+    for i in number:
+        total += i
+    return total
+
+
+print(sum_of_numbers(num))
+
+# Calculate the sum of numbers within an array.
---
+++ Module4/Module4C/Challenge/challenge3.py
@@ -0,0 +1,6 @@
+for i in range(100):
+    if i % 2 == 0:
+        print(i)
+
+
+#Print all even numbers from 0 – 100
