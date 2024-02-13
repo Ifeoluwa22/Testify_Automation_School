a960b5a915b18578e553c7bda93ba8118a983929
ifeoluwa22 <adeboyeifeoluwa@gmail.com>
2024-02-13 18:24:14 +0100

    four and five

---
+++ Module4/Module4C/Challenge/challenge4.py
@@ -0,0 +1,11 @@
+def multiplication(number):
+    for i in range(1, 13):
+        for j in range(1, number+1):
+            multi = i * j
+        print(multi)
+
+
+multiplication(100)
+
+
+# Print a table containing multiplication tables.
---
+++ Module4/Module4C/Challenge/challenge5.py
@@ -0,0 +1,12 @@
+students = ["Temi", "Feyi", "Tamilore", "Uchechi", "Opolo", "Idrisa", "Damson", "Michaela", "Harvey", "Animashaun"]
+
+
+def the_reverse(rev):
+    rev.reverse()
+    return rev
+
+
+print(the_reverse(students))
+
+
+# Create a function that reverses an array.
